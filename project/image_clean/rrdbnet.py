5705c134d024208c960637583081568121512909
delldu <18588220928@163.com>
2022-09-13 00:27:12 +0800

    format source code

--- project/image_clean/rrdbnet.py
+++ project/image_clean/rrdbnet.py
@@ -13,49 +13,9 @@
 import torch
 from torch import nn as nn
 from torch.nn import functional as F
-# from torch.nn import init as init
-# from torch.nn.modules.batchnorm import _BatchNorm
-
-
-# def default_init_weights(module_list, scale=1, bias_fill=0, **kwargs):
-#     """Initialize network weights.
-
-#     Args:
-#         module_list (list[nn.Module] | nn.Module): Modules to be initialized.
-#         scale (float): Scale initialized weights, especially for residual
-#             blocks. Default: 1.
-#         bias_fill (float): The value to fill bias. Default: 0
-#         kwargs (dict): Other arguments for initialization function.
-#     """
-#     if not isinstance(module_list, list):
-#         module_list = [module_list]
-#     for module in module_list:
-#         for m in module.modules():
-#             if isinstance(m, nn.Conv2d):
-#                 init.kaiming_normal_(m.weight, **kwargs)
-#                 m.weight.data *= scale
-#                 if m.bias is not None:
-#                     m.bias.data.fill_(bias_fill)
-#             elif isinstance(m, nn.Linear):
-#                 init.kaiming_normal_(m.weight, **kwargs)
-#                 m.weight.data *= scale
-#                 if m.bias is not None:
-#                     m.bias.data.fill_(bias_fill)
-#             elif isinstance(m, _BatchNorm):
-#                 init.constant_(m.weight, 1)
-#                 if m.bias is not None:
-#                     m.bias.data.fill_(bias_fill)
-
 
 def pixel_unshuffle(x, scale: int):
     """Pixel unshuffle.
-
-    Args:
-        x (Tensor): Input feature with shape (b, c, hh, hw).
-        scale (int): Downsample ratio.
-
-    Returns:
-        Tensor: the pixel unshuffled feature.
     """
     b, c, hh, hw = x.size()
     out_channel = int(c * (scale ** 2))
@@ -68,13 +28,6 @@ def pixel_unshuffle(x, scale: int):
 
 def make_layer(basic_block, num_basic_block, **kwarg):
     """Make layers by stacking the same blocks.
-
-    Args:
-        basic_block (nn.module): nn.module class for basic block.
-        num_basic_block (int): number of blocks.
-
-    Returns:
-        nn.Sequential: Stacked blocks in nn.Sequential.
     """
     layers = []
     for _ in range(num_basic_block):
@@ -84,12 +37,6 @@ def make_layer(basic_block, num_basic_block, **kwarg):
 
 class ResidualDenseBlock(nn.Module):
     """Residual Dense Block.
-
-    Used in RRDB block in ESRGAN.
-
-    Args:
-        num_feat (int): Channel number of intermediate features.
-        num_grow_ch (int): Channels for each growth.
     """
 
     def __init__(self, num_feat=64, num_grow_ch=32):
@@ -117,12 +64,6 @@ class ResidualDenseBlock(nn.Module):
 
 class RRDB(nn.Module):
     """Residual in Residual Dense Block.
-
-    Used in RRDB-Net in ESRGAN.
-
-    Args:
-        num_feat (int): Channel number of intermediate features.
-        num_grow_ch (int): Channels for each growth.
     """
 
     def __init__(self, num_feat, num_grow_ch=32):
@@ -144,19 +85,6 @@ class RRDBNet(nn.Module):
     in ESRGAN.
 
     ESRGAN: Enhanced Super-Resolution Generative Adversarial Networks.
-
-    We extend ESRGAN for scale x2 and scale x1.
-    Note: This is one option for scale 1, scale 2 in RRDBNet.
-    We first employ the pixel-unshuffle (an inverse operation of pixelshuffle to reduce the spatial size
-    and enlarge the channel size before feeding inputs into the main ESRGAN architecture.
-
-    Args:
-        num_in_ch (int): Channel number of inputs.
-        num_out_ch (int): Channel number of outputs.
-        num_feat (int): Channel number of intermediate features.
-            Default: 64
-        num_block (int): Block number in the trunk network. Defaults: 23
-        num_grow_ch (int): Channels for each growth. Default: 32.
     """
 
     def __init__(self, num_in_ch, num_out_ch, scale=4, num_feat=64, num_block=23, num_grow_ch=32):
